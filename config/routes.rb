eb995628956c5962658b76c9e01cbe2df1149fe2
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-01 15:41:18 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :incidents, except: [:new, :edit] do
+    resources :cases, only: [:index, :create]
+  end
 end
