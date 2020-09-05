43a0ba9fe60f73519fa9901c7709fe920fb7bc5e
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-05 14:36:55 -0400

    Render case with media_url

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,6 +1,6 @@
 class CasesController < ApplicationController
   def index
     @cases = Case.where(incident_id: params[:incident_id])
-    render json: @cases
+    render json: @cases, methods: [:media_url]
   end
 end
