274adb64ef0ea91af7e034ba7d7d96bd1ffa698d
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-05 14:36:39 -0400

    Configure case with active storage blob

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,3 +1,8 @@
 class Case < ApplicationRecord
   belongs_to :incident
+  has_one_attached :media
+
+  def media_url
+    Rails.application.routes.url_helpers.rails_blob_path(self.media, only_path: true)
+  end
 end
