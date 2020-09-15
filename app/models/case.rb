51015bedb981dbbcf668b45cfce120c9eaa54cef
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-15 15:08:03 -0400

    Add validations for title and description

--- app/models/case.rb
+++ app/models/case.rb
@@ -2,6 +2,9 @@ class Case < ApplicationRecord
   belongs_to :incident
   has_one_attached :media
 
+  validates :title, presence: true
+  validates :description, presence: true
+
   def media_url
     Rails.application.routes.url_helpers.rails_blob_path(self.media, only_path: true)
   end
