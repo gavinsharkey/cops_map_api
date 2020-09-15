3dd7fd4d4d3716ea96143605036671d900f534a5
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-15 15:07:48 -0400

    add dependent destroy for cases add cases_attributes= method

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,7 +1,13 @@
 class Incident < ApplicationRecord
-  has_many :cases
+  has_many :cases, dependent: :destroy
   validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
   validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+
+  def cases_attributes=(cases_data)
+    cases_data.each do |case_data|
+      self.cases.build(case_data)
+    end
+  end
 
   def self.in_radius(lat, long, radius)
     lat = lat.to_f
