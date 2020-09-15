9e25e238962b3e5b98528165d215ebbf3c1461d4
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-15 15:07:14 -0400

    add create and incident_params

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -3,4 +3,20 @@ class IncidentsController < ApplicationController
     @incidents = Incident.in_radius(params[:latitude], params[:longitude], params[:radius])
     render json: @incidents
   end
+
+  def create
+    byebug
+    # @incident = Incident.new(incident_params)
+    # if @incident.save
+    #   render json: @incident, include: { cases: { methods: [:media_url] } }
+    # else
+    #   render json: { errors: @incident.errors.full_messages }
+    # end
+  end
+
+  private
+
+  def incident_params
+    params.require(:incident).permit(:latitude, :longitude, :description, cases_attributes: [:title, :description, :media])
+  end
 end
