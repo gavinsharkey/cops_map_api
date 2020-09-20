36fd865b321ce1a411fb4578325b8dbf7e6afc88
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-20 13:58:38 -0400

    Unnested cases routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :incidents, except: [:new, :edit] do
-    resources :cases, only: [:index, :create]
-  end
+  resources :incidents, except: [:new, :edit]
+  resources :cases, only: [:index, :create]
 end
