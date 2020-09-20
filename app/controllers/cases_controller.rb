be35d4cd99d4358512a6907f484844102a4348b4
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-20 13:58:51 -0400

    Add create and case_params methods

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,6 +1,21 @@
 class CasesController < ApplicationController
   def index
-    @cases = Case.where(incident_id: params[:incident_id])
+    @cases = Case.by_incident(params[:incident_id])
     render json: @cases, methods: [:media_url]
+  end
+
+  def create
+    @case = Case.new(case_params)
+    if @case.save
+      render json: @case, methods: [:media_url]
+    else
+      render json: { errors: @case.errors.full_messages }
+    end
+  end
+
+  private
+
+  def case_params
+    params.require(:case).permit(:title, :description, :incident_id, :media)
   end
 end
