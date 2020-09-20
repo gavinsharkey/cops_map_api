58f7c2431373e714ffd6a797407f9fc801fe1b6e
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-20 13:59:24 -0400

    Add by_incident method for cases

--- app/models/case.rb
+++ app/models/case.rb
@@ -5,6 +5,10 @@ class Case < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
 
+  def self.by_incident(incident_id)
+    where(incident_id: incident_id).order(created_at: :desc)
+  end
+
   def media_url
     Rails.application.routes.url_helpers.rails_blob_path(self.media, only_path: true)
   end
